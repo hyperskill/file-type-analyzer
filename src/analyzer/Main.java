165aafedc05c5ba403ed009d78d7a71bb1ea40db
oleksii <aleksey.petenko@gmail.com>
2019-02-09 16:13:16 +0200

    First stage impl.

--- src/analyzer/Main.java
+++ src/analyzer/Main.java
@@ -1,7 +1,17 @@
 package analyzer;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      FileTypePatterns fileTypePatterns = new FileTypePatterns(
+          Arrays.copyOfRange(args, 1, args.length));
+
+      String fileType = fileTypePatterns.checkFileType(args[0]);
+      System.out.println(fileType);
     }
+  }
 }
